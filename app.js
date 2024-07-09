44aff8480a2070a44f0de5a978d1cb83a961552d
Yashika Aggarwal <yashikaaggarwal0107@gmail.com>
2024-07-09 16:29:39 +0530

    WanderLust

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-//dotenv ek npm package h jo .env se enviroment variables ko read krta h
 if (process.env.NODE_ENV != "production") {
     require('dotenv').config();
 }
@@ -13,8 +12,6 @@ const ExpressError = require("./util/ExpressError.js");
 const session = require("express-session");
 const MongoStore = require('connect-mongo');
 
-
-
 const listingRouter = require("./routes/listing.js");
 const reviewRouter = require("./routes/review.js");
 const userRouter = require("./routes/user.js");
@@ -24,18 +21,14 @@ const passport = require("passport");
 const LocalStrategy = require("passport-local");
 const User = require("./models/user.js");
 
-//Code for connection
-//const MONGO_URL="mongodb://127.0.0.1:27017/wanderlust";
-
 const dbUrl = process.env.ATLASDB_URL;
 
 main().then(() => {
     console.log("connected to db");
 }).catch(err => {
-    console.log(err);
+    console.error("Database connection error:", err);
 });
 
-//ab mongoose atlas db se connect kregaa
 async function main() {
     await mongoose.connect(dbUrl);
 }
@@ -49,19 +42,17 @@ app.use(express.static(path.join(__dirname, "/public")));
 app.use(express.static(path.join(__dirname, "/public/css")));
 app.use(express.static(path.join(__dirname, "/public/js")));
 
-
-//ye mongo connect h jo session ki info ko store krtaa hh
 const store = MongoStore.create({
     mongoUrl: dbUrl,
     crypto: {
         secret: process.env.SECRET,
     },
     touchAfter: 24 * 3600,
-})
+});
 
-store.on("error", () => {
-    console.log("Error in Mongo session store", err);
-})
+store.on("error", (err) => {
+    console.error("Error in Mongo session store", err);
+});
 
 const sessionOptions = {
     store,
@@ -72,13 +63,8 @@ const sessionOptions = {
         expires: Date.now() + 7 * 24 * 60 * 60 * 1000,
         maxAge: 7 * 24 * 60 * 60 * 1000,
         httpOnly: true,
-    }
+    },
 };
-
-// app.get("/",(req,res)=>{
-//     res.send("hi,i am root");
-// });
-
 
 app.use(session(sessionOptions));
 app.use(flash());
@@ -90,7 +76,6 @@ passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
-//defining middleware
 app.use((req, res, next) => {
     res.locals.success = req.flash("success");
     res.locals.error = req.flash("error");
@@ -98,22 +83,6 @@ app.use((req, res, next) => {
     next();
 });
 
-// app.get("/demouser", async(req,res)=>{
-//     let fakeUser= new User({
-//         email:"student@gmail.com",
-//         username:"delta-student"
-//     });
-
-//ismein user ka register method h vo db mein automatically fakeUser ko
-//store krvadega aur password ko helloword
-//     let registeredUser=await User.register(fakeUser,"helloword");
-//     res.send(registeredUser);
-// })
-
-
-
-//lisitng ka sara code lisitng.js mein shift kiya hua h 
-//aur line 50 se usko acces kiya jaa rha h
 app.use("/listings", listingRouter);
 app.use("/listings/:id/reviews", reviewRouter);
 app.use("/", userRouter);
@@ -124,10 +93,11 @@ app.all("*", (req, res, next) => {
 
 app.use((err, req, res, next) => {
     let { statusCode = 500, message = "Something went wrong" } = err;
-    // res.status(statusCode).render("error.ejs", { message });
-    // res.status(statusCode).send(message);
+    console.error(err);
+    res.status(statusCode).send(message);
 });
 
-app.listen(3000, () => {
-    console.log("server is listening to port 3000");
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`server is listening to port ${port}`);
 });
